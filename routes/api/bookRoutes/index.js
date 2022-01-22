d6072f588933202b184ca16cf8bb872d5f490131
Rick Ansay <rick.ansay@gmail.com>
2022-01-22 14:14:43 -0700

    First commit

--- routes/api/bookRoutes/index.js
+++ routes/api/bookRoutes/index.js
@@ -2,6 +2,54 @@ const router = require('express').Router();
 
 const Book = require('../../../models/Book');
 
+router.get('/', async (req, res) => {
+    try {
+        const books = await Book.findAll();
+        res.json(books);
+    } catch (e) {
+        res.json(e);
+    }
+});
+
+router.get('/:bookId', async (req, res) => {
+    try {
+        const book = await Book.findByPk(req.params.bookId);
+        res.json(book);
+    } catch (e) {
+        res.json(e);
+    }
+});
+
+router.patch('/:bookId', async (req, res) => {
+    const {
+        title,
+        author,
+        isbn,
+        pages,
+        edition,
+        isPaperback,
+    } = req.body;
+    try {
+        await Book.update(
+            {
+                title,
+                author,
+                isbn,
+                pages,
+                edition,
+                isPaperback,
+            },
+            {where: {
+                id: req.params.bookId
+            },
+        });
+        const updatedBook = await Book.findByPk(req.params.bookId);
+        res.json(updatedBook);
+    } catch (e) {
+        res.json(e);
+    }
+})
+
 router.post('/', async (req, res) => {
     const { title, author } = req.body;
     try {
--- server.js
+++ server.js
@@ -2,12 +2,14 @@ const express = require('express');
 
 const sequelize = require('./config');
 
-const Book = require('./models/Book');
+const routes = require('./routes');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
 
 // Connect to the database prior to starting our server
 
